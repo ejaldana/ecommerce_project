e32ba4b81270dd55f076e789a1a60f611f4b650f
Eric Aldana <ejay918@gmail.com>
2017-03-17 23:36:11 +0000

    Modified models

---
+++ app/admin/line_item.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register LineItem do
+  permit_params :quantity, :price, :created_at, :updated_at, :product_id, :order_id
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
