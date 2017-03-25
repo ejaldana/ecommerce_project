4a4af9ab4f4b988c0a024798336e0dd3a49fa15b
Eric Aldana <ejay918@gmail.com>
2017-03-25 00:20:49 +0000

    Added HTML select for categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,22 @@
 class ProductsController < ApplicationController
   def index
     if params[:search]
-      @products = Product.search(params[:search]).page(params[:page]).per(5)
-      #second search for category name
-      if @products.empty?
-        #grab the id of category then find all products with that category
-        category = Category.where('name LIKE ?', "%#{params[:search]}%").first
-        @products = Product.where("category_id = #{category.id}").page(params[:page]).per(5)
+      if params[:category_type] == 'all'
+        @products = Product.search(params[:search]).page(params[:page]).per(5)
+      else
+        @products = Product.where('category_id LIKE ?', "%#{params[:category_type]}%").search(params[:search]).page(params[:page]).per(5)
       end
-    elsif params[:category_id]
-      @products = Product.where("category_id = #{params[:category_id]}").page(params[:page]).per(5)
+
+      #second search for category name
+
+
+      #if @products.empty?
+        #grab the id of category then find all products with that category
+        #category = Category.where('name LIKE ?', "%#{params[:search]}%").first
+        #@products = Product.where("category_id = #{category.id}").page(params[:page]).per(5)
+      #end
+  #  elsif params[:category_type]
+    #  @products = Product.where("category_id = #{params[:category_id]}").page(params[:page]).per(5)
     else
       @products = Product.page(params[:page]).per(5)
     end
