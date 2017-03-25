5bd1fcbb3fb448107f60a9be298decf94549834e
Eric Aldana <ejay918@gmail.com>
2017-03-25 03:03:56 +0000

    Functionality to register user in the database

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,12 +1,18 @@
 class RegisterController < ApplicationController
   def index
-    User.create(username: params[:username],
-                password: params[:password])
+    if params[:username]
+      user = User.create(username: params[:username],
+                  password: params[:password])
 
-    Customer.create(name: params[:name],
-                    address: params[:address],
-                    city: params[:city],
-                    postalcode: params[:postalcode],
-                    email: params[:email])
+      customer = Customer.create(name: params[:name],
+                      address: params[:address],
+                      city: params[:city],
+                      postalcode: params[:postalcode],
+                      email: params[:email],
+                      user_id: user.id,
+                      province_id: params[:province])
+
+      redirect_to :controller => 'home', :action => 'index'
+    end
   end
 end
